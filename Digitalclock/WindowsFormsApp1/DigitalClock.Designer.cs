7f1ebe67863d7c977403dda4e20afd8581bc578c
ShayestehHS <hosseinshayesteh47.hs@gmail.com>
2021-02-04 20:47:52 +0330

    Version 1.0

---
+++ Digitalclock/WindowsFormsApp1/DigitalClock.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class DigitalClock
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DigitalClock));
+            this.lblHoure = new System.Windows.Forms.Label();
+            this.lblSecond = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.lblDay = new System.Windows.Forms.Label();
+            this.Timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblHoure
+            // 
+            this.lblHoure.BackColor = System.Drawing.Color.Transparent;
+            this.lblHoure.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblHoure.Font = new System.Drawing.Font("DS-Digital", 72F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHoure.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblHoure.Location = new System.Drawing.Point(271, 28);
+            this.lblHoure.Name = "lblHoure";
+            this.lblHoure.Size = new System.Drawing.Size(316, 117);
+            this.lblHoure.TabIndex = 0;
+            this.lblHoure.Text = "00:00";
+            // 
+            // lblSecond
+            // 
+            this.lblSecond.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblSecond.Font = new System.Drawing.Font("DS-Digital", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSecond.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblSecond.Location = new System.Drawing.Point(572, 96);
+            this.lblSecond.Name = "lblSecond";
+            this.lblSecond.Size = new System.Drawing.Size(57, 39);
+            this.lblSecond.TabIndex = 1;
+            this.lblSecond.Text = "00";
+            // 
+            // lblDate
+            // 
+            this.lblDate.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblDate.Font = new System.Drawing.Font("DS-Digital", 48F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDate.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblDate.Location = new System.Drawing.Point(12, 189);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(407, 81);
+            this.lblDate.TabIndex = 2;
+            this.lblDate.Text = "---  __ ____";
+            // 
+            // lblDay
+            // 
+            this.lblDay.AutoSize = true;
+            this.lblDay.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblDay.Font = new System.Drawing.Font("DS-Digital", 48F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDay.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblDay.Location = new System.Drawing.Point(495, 189);
+            this.lblDay.Name = "lblDay";
+            this.lblDay.Size = new System.Drawing.Size(333, 81);
+            this.lblDay.TabIndex = 3;
+            this.lblDay.Text = "---------";
+            // 
+            // Timer
+            // 
+            this.Timer.Enabled = true;
+            this.Timer.Interval = 1000;
+            this.Timer.Tick += new System.EventHandler(this.Timer_Tick);
+            // 
+            // DigitalClock
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(873, 330);
+            this.Controls.Add(this.lblSecond);
+            this.Controls.Add(this.lblHoure);
+            this.Controls.Add(this.lblDay);
+            this.Controls.Add(this.lblDate);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "DigitalClock";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Digtal Clock";
+            this.Load += new System.EventHandler(this.DigitalClock_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHoure;
+        private System.Windows.Forms.Label lblSecond;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.Label lblDay;
+        private System.Windows.Forms.Timer Timer;
+    }
+}
+
---
+++ Digitalclock/WindowsFormsApp1/DigitalClock.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class DigitalClock : Form
+    {
+        public DigitalClock()
+        {
+            InitializeComponent();
+        }
+
+        private void DigitalClock_Load(object sender, EventArgs e)
+        {
+            Timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            lblSecond.Text = DateTime.Now.ToString("ss");
+            lblHoure.Text = DateTime.Now.ToString("HH:mm");
+            lblDay.Text = DateTime.Now.ToString("dddd");
+            lblDate.Text = DateTime.Now.ToString("MMM dd yyyy");
+        }
+    }
+}
